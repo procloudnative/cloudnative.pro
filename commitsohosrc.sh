38472d0aaac9faafdcf4f0388c4c2ab6802bf656
xiaobo <zeng.xiaobo@h3c.com>
2021-01-22 16:18:17 +0800

    rebuilding site 2021年 1月22日 星期五 16时18分17秒 CST

---
+++ commitsohosrc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mCommitting updates to GitHub...\033[0m\n"
+
+cd themes/soho
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
